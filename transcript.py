b2a2edca6c5f1cf332b113c4a4debdd69eb72e1c
Okwuobu Chifurumnanya <105526312+arnoldddude@users.noreply.github.com>
2025-03-11 11:52:22 -0700

    code update

--- transcript.py
+++ transcript.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import os
 import speech_recognition
-from pytube import YouTube
+from pytubefix import YouTube
 from moviepy import *
 
 
@@ -18,14 +18,20 @@ class Transcript:
         try:
             yt = YouTube(self.link_path)
             ys = yt.streams.get_highest_resolution()
-            self.video_path = f"{CWD}/{yt.streams[0].default_name}"
+            print(CWD)
+            self.video_path = CWD
+            print("Starting download....")
             ys.download(output_path=self.video_path)
+            print("Download complete!")
+            self.video_path = f"{CWD}/{yt.title}.mp4"
+            self.audio_path = f"{CWD}/{yt.title}.wav"
         except Exception as e:
             print(f"An error occured: {e}")
 
     # convert the video to audio, and then extract the text
     def video_to_text(self):
         try:
+            print(self.video_path)
             # extract audio from video
             video_clip = VideoFileClip(self.video_path)
             video_clip.audio.write_audiofile(self.audio_path)
@@ -35,7 +41,22 @@ class Transcript:
             with speech_recognition.AudioFile(self.audio_path) as source:
                 data = recognizer.record(source)
             #     convert speech to text
-            text = recognizer.recognize_google(data)
+            # continue from here the recognizer is not working or is not being instantiated
+            print(data)
+            print(recognizer)
+            text = recognizer.recognize_google(data, key)
+            # recognize speech using Google Speech Recognition
+            try:
+                # for testing purposes, we're just using the default API key
+                # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+                # instead of `r.recognize_google(audio)`
+                text = recognizer.recognize_google(data, key=)
+            except speech_recognition.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except speech_recognition.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+            print("male")
             # video_clip.close()
             print(f"Successfully converted '{self.video_path}' to '{self.audio_path}'")
 
